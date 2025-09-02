0cddc86b0ff4032420d9a835f93fc6a613eb1c7b
Nash Aaron <ishowsus@gmail.com>
2025-09-02 09:46:34 +0800

    index and create were changed

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCW98WyxqrqrRgxHu9cTO_ZGxfosrMMgT8",
+  authDomain: "naml-mobile-app---tracker.firebaseapp.com",
+  projectId: "naml-mobile-app---tracker",
+  storageBucket: "naml-mobile-app---tracker.firebasestorage.app",
+  messagingSenderId: "821155748360",
+  appId: "1:821155748360:web:2c51635d4a75f2478e0fbd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
